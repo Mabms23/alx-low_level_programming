adbd62a3bd8f3feab78cbb9c120d8aad3f231583
Mabms23 <msalih@mailfence.com>
2023-08-17 05:21:51 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,8 +5,8 @@
  *
  * Return: 1 (Success)
  */
-int main(void)
+int main(void) 
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",stdout);
+	fputs ("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
