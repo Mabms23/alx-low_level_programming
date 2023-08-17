4969310a0274df6e01999de89ad3cecd3f9ab0fd
Mabms23 <msalih@mailfence.com>
2023-08-17 00:56:43 -0700

    use print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,7 @@
-#include<stdio.h>
-/**
- * main - Entry point
- * Return: Always 0
- */
+#include <stdio.h>
+
 int main(void)
-	{
-		printf(" with proper grammar, but the outcome is a piece of art,");
-		return (0);
-	}
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
