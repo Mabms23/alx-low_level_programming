769cc2007c5dd63b2dc605e562922ebef3ab3b3e
Mabms23 <msalih@mailfence.com>
2023-08-21 02:31:50 -0700

    first header line

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,22 @@
-#include<stdio.h>
-#include "_putchar.h"
+#include "main.h"
 
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * main- Entry point
+ * Description:a function called by the header file
+ * Return  :Always  0 (success)
  */
 
 int main(void)
 {
-	char word[8] = "_putchar";
-	int i;
-
-	for (i = 0; i < 9; i++)
-	{
-		_putchar(word[i]);
-	}
-	_putchar('/n');
+	_putchar("_");
+	_putchar("p");
+	_putchar("u");
+	_putchar("t");
+	_putchar("c");
+	_putchar("h");
+	_putchar("a");
+	_putchar("r");
+	_putchar("\n");
 
 	return (0);
 }
