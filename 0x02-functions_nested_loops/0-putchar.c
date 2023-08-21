4b4234b1459ea0a3e91c55668b49ebaab9add476
Mabms23 <msalih@mailfence.com>
2023-08-21 08:29:29 -0700

    update

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,21 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include "main.h"
+
 /**
- * main - Entry point
- *
- * Return: Always 0 (success)
+ * main- Entry point
+ * Description:a function called by the header file
+ * Return  :Always  0 (success)
  */
 
 int main(void)
 {
-
-	printf("_putchar\n");
+	_putchar("_");
+	_putchar("p");
+	_putchar("u");
+	_putchar("t");
+	_putchar("c");
+	_putchar("h");
+	_putchar("a");
+	_putchar("r");
+	_putchar("\n");
 	return (0);
 }
-
-
